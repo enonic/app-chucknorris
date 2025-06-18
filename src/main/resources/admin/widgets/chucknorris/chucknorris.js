7edd6b19c7a531afa5efb0b9d07f31a300a5b499
rymsha <sry@enonic.com>
2025-06-18 14:37:12 +0200

    XP8 changes

--- src/main/resources/admin/widgets/chucknorris/chucknorris.js
+++ src/main/resources/admin/widgets/chucknorris/chucknorris.js
@@ -1,16 +1,28 @@
 const libs = {
-    thymeleaf: require('/lib/thymeleaf'),
-    httpClient: require('/lib/http-client')
+    mustache: require('/lib/mustache'),
+    httpClient: require('/lib/http-client'),
+    static: require('/lib/enonic/static')
 };
 
-exports.get = handleGet;
+const router = require('/lib/router')()
 
-function handleGet(req) {
+const BASE_PATH = `/${app.name}/chucknorris`;
+const STATIC_BASE_PATH = `${BASE_PATH}/_static`;
+
+exports.all = function (req) {
+    return router.dispatch(req);
+};
+
+router.get(`${BASE_PATH}`, (req) => {
     const view = resolve('chucknorris.html');
     const model = createModel(req);
 
     function createModel(req) {
-        var model = {};
+        const handlerUrl = req.path;
+        const model = {
+            css: `${handlerUrl}/_static/css/chucknorris.css`,
+            image: `${handlerUrl}/_static/img/chucknorris.svg`,
+        };
 
         model.errors = [];
 
@@ -51,6 +63,18 @@ function handleGet(req) {
     }
 
     return {
-        body: libs.thymeleaf.render(view, model)
+        body: libs.mustache.render(view, model)
     };
-}
+});
+
+router.get(`${STATIC_BASE_PATH}/{path:.*}`, (request) => {
+    return libs.static.requestHandler(
+        request,
+        {
+            cacheControl: () => libs.static.RESPONSE_CACHE_CONTROL.SAFE,
+            index: false,
+            root: '/assets',
+            relativePath: libs.static.mappedRelativePath(STATIC_BASE_PATH),
+        }
+    );
+});
